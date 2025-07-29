5963dc6d52de660d00b8258ea67f41119c2462c2
erickbauerv <contatoerickbauer@gmail.com>
2025-07-29 17:52:19 -0300

    Implementado o MapaComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { DashboardMaquinasComponent } from './components/dashboard-maquinas/dashboard-maquinas.component';
 import { CadastroMaquinasComponent } from './components/cadastro-maquinas/cadastro-maquinas.component';
 import { DetalhesMaquinaComponent } from './components/detalhes-maquina/detalhes-maquina.component';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardMaquinasComponent },
     { path: 'cadastro', component: CadastroMaquinasComponent },
     { path: 'maquina/:id', component: DetalhesMaquinaComponent },
+    { path: 'mapa', component: MapaComponent },
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
